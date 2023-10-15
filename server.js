2ae1e1a1b069209bd9dbf84e0c44585b2c6847b9
Krier <vinoshan@outlook.com>
2023-10-15 00:18:22 -0400

    Added Starter Code

---
+++ server.js
@@ -0,0 +1,18 @@
+//Vars: express, db, routes, PORT, app
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//Middleware: express.urlencoded, express.json, routes
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+//Connect to db, listen on PORT
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`🚀 API server is running on port ${PORT}!`);
+  });
+});
